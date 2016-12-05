24611710bdeb80480ea9fef071606ee64d3ee8c6
mihajlo <mihajlo88@gmail.com>
2016-12-05 16:58:45 +0100

    sredjivanje interfejsa; ubacivanje podataka o koalicijama

--- viz/map.js
+++ viz/map.js
@@ -1,3 +1,24 @@
+$(function() {
+
+	//ucitaj podatke po koaliciji
+	
+	poKoaliciji();
+})
+
+function poKoaliciji(argument) {
+	$("#koalicija").change(function(event) {
+		var koal = event.target.value;
+
+		//filtriraj skup podataka
+		//oboj mapu prema filtriranim opstinama 
+		   //debugger;
+		//console.log(podaci);   
+	 	//random oboj opstine
+	 	
+	});
+}
+
+
 function bojenje(argument) {
 	
 }
