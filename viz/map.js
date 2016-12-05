107aa1b354551c47fa878ab60d35b20da043175a
mihajlo <mihajlo88@gmail.com>
2016-12-05 17:00:30 +0100

    sredjivanje interfejsa; ubacivanje podataka o koalicijama

--- viz/map.js
+++ viz/map.js
@@ -9,6 +9,8 @@ function poKoaliciji(argument) {
 	$("#koalicija").change(function(event) {
 		var koal = event.target.value;
 
+		//treba mapirati id-eve svg-a sa nazivima opstina
+
 		//filtriraj skup podataka
 		//oboj mapu prema filtriranim opstinama 
 		   //debugger;
