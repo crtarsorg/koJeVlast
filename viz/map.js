d57b58ea3d0b2ea514866ad01811fae59b867b8d
mihajlo <mihajlo88@gmail.com>
2016-12-19 12:13:31 +0100

    promenjen pristup podacima(centralizovano); struktura podataka koji su grupisani prema opstinama je mnogo komplikovana i problematicna za izracunavanja; ili praviti novi fajl sa procentima ili ucitavati nestruktuiran fajl

--- viz/map.js
+++ viz/map.js
@@ -1,7 +1,7 @@
 $(function() {
 
 	//ucitaj podatke po koaliciji
-	
+	//napraviti eksterno podatke grupisane po koalicijama
 	poKoaliciji();
 })
 
@@ -9,9 +9,29 @@ function poKoaliciji(argument) {
 	$("#koalicija").change(function(event) {
 		var koal = event.target.value;
 
+		//console.log(koal);
 		//treba mapirati id-eve svg-a sa nazivima opstina
 
 		//filtriraj skup podataka
+		var temp = Data.get();
+		
+		var arr = Object.keys(temp).map(function (key) { return temp[key]; });
+		
+		var filt = arr.filter(function(el) {
+			
+			var ukupno = el.length;
+
+			var broj = el.filter(function (el1) {
+				return el1.koalicija == koal;
+			}).length;
+
+			return (broj/ukupno) > 0.5; 
+			
+		})
+		//prodji kroz sve filtrirane i ofarbaj ih
+		console.log(filt);
+		debugger;
+		
 		//oboj mapu prema filtriranim opstinama 
 		   //debugger;
 		//console.log(podaci);   
