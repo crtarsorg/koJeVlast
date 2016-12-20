30b186f15415f25fd831f738befbf99941fd5c3a
mihajlo <mihajlo88@gmail.com>
2016-12-20 15:27:09 +0100

    razna selektovanja

--- viz/map.js
+++ viz/map.js
@@ -3,9 +3,11 @@ $(function() {
 	//ucitaj podatke po koaliciji
 	//napraviti eksterno podatke grupisane po koalicijama
 	poKoaliciji();
+	poStranci();
+	poRegionu();
 })
 
-function poKoaliciji(argument) {
+function poKoaliciji() {
 	$("#koalicija").change(function(event) {
 		var koal = event.target.value;
 
@@ -44,12 +46,9 @@ function poKoaliciji(argument) {
 
 function bojenje(unosi) {
 
-	debugger;
+	//izbrisi prethodnu selekciju	
 	resetColors();
-
-
-	//izbrisi prethodnu selekciju
-	//iskljuci hover opciju event
+	
 	
 	for (var i = 0; i < unosi.length; i++) {
 		var temp_op = unosi[i];
@@ -64,6 +63,56 @@ function bojenje(unosi) {
 	}
 }
 
+function poStranci(argument) {
+	$("#stranka").change(function(event) {
+		var str = event.target.value;
+
+		//filtriraj skup podataka
+		var temp = Data.get();
+		//var arr = Object.keys(temp).map(function (key) { return temp[key]; });
+		
+		var filt = temp.filter(function(el) {
+			var temp_el = el.podaci;
+			var ukupno = temp_el.length;
+
+			var broj = temp_el.filter(function (el1) {
+				return el1.stranka == str;
+			}).length;
+
+			return (broj) > 0; ///ukupno
+			
+		})
+		//prodji kroz sve filtrirane i ofarbaj ih
+		/*console.log(filt);
+		debugger;*/
+
+		bojenje(filt);
+		
+		//oboj mapu prema filtriranim opstinama 
+		   //debugger;
+		//console.log(podaci);   
+	 	//random oboj opstine
+	 	
+	});
+}
+
+
+
+function poRegionu() {
+	$("#regioni").change(function(event) {
+		var region = event.target.value;
+
+		//daj sve opstine iz tog regiona
+		resetColors();
+		
+		$("g:not(#granice)[id*='"+region+"'] *").children().css("fill","red");
+		debugger;
+		
+		//oboj mapu prema filtriranim opstinama 
+		   //debugger;
+	 	
+	});
+}
 function srafiranje(argument) {
 	
 }
