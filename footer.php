9bbee593eaddd8be89afeb2d8b17d2842cce2d14
mihajlo <mihajlo88@gmail.com>
2017-06-28 17:51:49 +0200

    izbacivanje beograda iz liste osptina 1

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 	<script src="static/data.js?2"></script>
 	<script src="static/helpers.js?4"></script>
 	<script src="static/eventHandlers.js?2"></script>
-	<script src="static/script-index.js?12"></script>
+	<script src="static/script-index.js?13"></script>
 
 	</body>
 </html>
