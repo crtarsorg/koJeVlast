77d35c3c385614959fbafbf32825c1d23b064291
XoDev <misha@xodev.com>
2017-07-10 19:18:49 +0200

    Randomise script-index loading string - disable cache

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 	<script src="static/data.js?2"></script>
 	<script src="static/helpers.js?4"></script>
 	<script src="static/eventHandlers.js?6"></script>
-	<script src="static/script-index.js?13"></script>
+	<script src="static/script-index.js?<?php echo rand(11111,99999); ?>"></script>
 
 	</body>
 </html>
