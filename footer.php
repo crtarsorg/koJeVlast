a11128ddfd1946e541dcbc7ba7837871b05dce87
mihajlo <mihajlo88@gmail.com>
2017-07-25 15:34:23 +0200

    nestajanje opisa, js inc

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 	</main>
 
 	<script src="static/data.js?2"></script>
-	<script src="static/helpers.js?4"></script>
+	<script src="static/helpers.js?5"></script>
 	<script src="static/eventHandlers.js?7"></script>
 	<script src="static/script-index.js?<?php echo rand(11111,99999); ?>"></script>
 
