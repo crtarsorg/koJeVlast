3efdf6de2a14d652172eb4310fdd971b41a0f0d9
mihajlo <mihajlo88@gmail.com>
2017-08-14 11:56:07 +0200

    nestala map.js

--- footer.php
+++ footer.php
@@ -4,6 +4,7 @@
 	<script src="static/helpers.js?15"></script>
 	<script src="static/eventHandlers.js?7"></script>
 	<script src="static/script-index.js?<?php echo rand(11111,99999); ?>"></script>
+		<script src="static/map.js?2"></script>
 
 	</body>
 </html>
