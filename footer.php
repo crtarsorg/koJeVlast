14298e2bac6183d3cc51a57417f376497b1f844e
mihajlo <mihajlo88@gmail.com>
2017-08-14 12:58:04 +0200

    inc data

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 	</main>
 
-	<script src="static/data.js?2"></script>
+	<script src="static/data.js?4"></script>
 	<script src="static/helpers.js?15"></script>
 	<script src="static/eventHandlers.js?7"></script>
 	<script src="static/script-index.js?<?php echo rand(11111,99999); ?>"></script>
