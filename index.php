2e43a9ab9fed5a2fe0cac75eb38f153e94324533
mihajlo <mihajlo88@gmail.com>
2017-08-30 12:46:18 +0200

    dva puta ucitan jquery

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 	<?php include_once 'partials/head-part.html'; ?>
 
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
     <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css" />
     <script src="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js"></script>
     <style>
