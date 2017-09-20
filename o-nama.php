5812fe726080bdd2d05145000dd4305a9fd420e4
mihajlo <mihajlo88@gmail.com>
2017-09-20 18:34:59 +0200

    meni je centralizovan

--- o-nama.php
+++ o-nama.php
@@ -11,36 +11,7 @@
 
 <body>
 
-	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="/"><img src="static/icons/logo.svg"></a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav">
-                    <li><a href="/">Početna</a></li>
-                   <!--  <li><a href="partials/uporedjivanje.html">Uporedjivanje opstina</a></li> -->
-                    <li><a href="tabela.php">Tabele sa podacima</a></li>
-                    <li><a href="statistike.php">Statistike</a></li>
-                    <li><a href="vesti.php">Vesti</a></li>
-                    <li class="active"><a href="o-nama.php">O nama</a>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="prijavi"><a href="<?php echo 'http://'.$_SERVER["SERVER_NAME"].'/api/posaljitePromenu';?>">Prijavi promenu</a></li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-    </nav>
-    <main>
+	<?php include 'partials/nav.html'; ?>
 
 	<div class="container">
 		<div class="o-nama">
